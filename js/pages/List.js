8fb044a876ab9196724bc039244c75ccbad18820
Fe4rcasp1 <Fe4rcasp@gmail.com>
2026-09-14 07:14:39 -0400

    Update submission requirements and guidelines

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,28 +100,28 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Submit Veriications in the discord (link at top of website) and all completions using the google doc from the submit button.
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record.
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Verifications And Completions Must Have Clicks, Cheat Indicator, And Show The End Screen (Except Limitations Like Mobile, Mic Not Working, Or Not Having Mods.
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this.
                     </p>
                     <p>
                         The recording must also show the player hit the endwall, or the completion will be invalidated.
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        Do not use secret routes or bug routes.
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        Levels Can Be Beaten/Verified With 100% Noclip Accuracy If Enough Proof That It Was Legit.
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        This Is More A Statement Than A Rule But Everyones Username On The List Will Be Based Off Of Their In-Game Username So Private Dm CyrryGD Your In-Game Username If You See This (only if your in-game name is different from your discord name).
                     </p>
                 </div>
             </div>

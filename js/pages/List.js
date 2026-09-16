c1a344dfef45799f624e670c5001e936b66039af
Cyrry <codyray010311@gmail.com>
2026-09-16 03:57:24 -0500

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -128,7 +128,10 @@ export default {
                         Cyan = Mythic Rating
                     </p>
                     <p>
-                        <h4>Submission Requirements</h4> 
+                    
+                        <h4>Submission Requirements</h4>
+                        
+                    </p>
                     <p>
                         Submit Verifcations in the discord (link at the top of the website) and submit completions using the "submit record" button or in the discord.
                     </p>

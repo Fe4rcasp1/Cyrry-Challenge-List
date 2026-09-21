7688fdb5e30fd3547f492cf9f93ebe3eb78ccd3d
Fe4rcasp1 <Fe4rcasp@gmail.com>
2026-09-21 14:50:36 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,3 +19,35 @@ const router = VueRouter.createRouter({
 app.use(router);
 
 app.mount('#app');
+
+import routes from './routes.js';
+
+export const store = Vue.reactive({
+    dark: JSON.parse(localStorage.getItem('dark')) || false,
+
+    listType: localStorage.getItem('listType') || 'CCL',
+
+    lists: ['CCL', 'CPL', 'CIL', 'CPIL'],
+
+    setListType(type) {
+        this.listType = type;
+        localStorage.setItem('listType', type);
+    },
+
+    toggleDark() {
+        this.dark = !this.dark;
+        localStorage.setItem('dark', JSON.stringify(this.dark));
+    },
+});
+
+const app = Vue.createApp({
+    data: () => ({ store }),
+});
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+});
+
+app.use(router);
+app.mount('#app');
